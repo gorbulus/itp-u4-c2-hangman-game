01785278f794ae177f1c242ae05e09c15e35578d
William Ponton <waponton@gmail.com>
2019-02-16 05:03:56 +0000

    All tests passing

--- hangman/game.py
+++ hangman/game.py
@@ -14,22 +14,33 @@ def _get_random_word(list_of_words):
 def _mask_word(word):
     if not word:
         raise InvalidWordException()
-    return "*" * len(word)
+    return '*' * len(word)
+
 
 def _uncover_word(answer_word, masked_word, character):
     if not answer_word or not masked_word:
         raise InvalidWordException()
+
     if len(character) > 1:
         raise InvalidGuessedLetterException()
+
     if len(answer_word) != len(masked_word):
         raise InvalidWordException()
-    answer = answer_aord.lower()
+
+    answer = answer_word.lower()
+    if character.lower() not in answer:
+        return masked_word
+
+    new_word = ''
+
     for answer_char, masked_char in zip(answer, masked_word):
-        if character.lower() == answer_char
+        if character.lower() == answer_char:
             new_word += answer_char
         else:
             new_word += masked_char
+
     return new_word
+
 
 def _is_game_won(game):
     return game['answer_word'].lower() == game['masked_word'].lower()
@@ -40,28 +51,36 @@ def _is_game_lost(game):
 
 
 def _is_game_finished(game):
-return _is_game_lost(game) or _is_game_won(game)
+    return _is_game_lost(game) or _is_game_won(game)
+
 
 def guess_letter(game, letter):
     letter = letter.lower()
-    if letter in game["previous_guesses"]:
+    if letter in game['previous_guesses']:
         raise InvalidGuessedLetterException()
-    if _is_game_finished(game):
+
+    if _is_game_finished(game):  # Already Won
         raise GameFinishedException()
-    previous_masked = game["masked_word"]
-    new_masked = _uncover_word(game["answer_word"], previous_masked, letter)
-    
+
+    previous_masked = game['masked_word']
+    new_masked = _uncover_word(game['answer_word'], previous_masked, letter)
+
     if previous_masked == new_masked:
-        game["remaining_misses"] -= 1
+        # This is a miss!
+        game['remaining_misses'] -= 1
     else:
-        game["masked_word"] = new_masked
-    game["previous_guesses"].append(letter)
-    
+        # This is a correct guess!
+        game['masked_word'] = new_masked
+
+    game['previous_guesses'].append(letter)
+
     if _is_game_won(game):
         raise GameWonException()
+
     if _is_game_lost(game):
+
         raise GameLostException()
-        
+
     return new_masked
 
 
@@ -72,10 +91,10 @@ def start_new_game(list_of_words=None, number_of_guesses=5):
     word_to_guess = _get_random_word(list_of_words)
     masked_word = _mask_word(word_to_guess)
     game = {
-        'answer_word': word_to_guess,
-        'masked_word': masked_word,
-        'previous_guesses': [],
-        'remaining_misses': number_of_guesses,
+            'answer_word': word_to_guess,
+            'masked_word': masked_word,
+            'previous_guesses': [],
+            'remaining_misses': number_of_guesses,
     }
-
+    
     return game
